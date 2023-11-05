75ae753d7d89c2178a0ec823676d2b786f289fec
HanniWalter <hannibali@gmx.de>
2023-11-05 15:48:19 +0100

    rabbitmq for frontpod

--- services/website/app.py
+++ services/website/app.py
@@ -1,46 +1,61 @@
+import os
+import json
 from flask import Flask, render_template, jsonify, request, abort
 import logging
-import requests
+import pika
+
 logging.basicConfig(level=logging.DEBUG)
-import json
 app = Flask(__name__)
+
 
 @app.route("/")
 def main_page():
     return render_template('index.html')
 
-@app.route("/searchflight", methods = ["POST"])
+
+@app.route("/searchflight", methods=["POST"])
 def searchflights():
     if request.method == "POST":
         value = request.get_json()
-        app.logger.info(value)
         response = jsonify({})
         response.headers.add('Access-Control-Allow-Origin', '*')
-        
-        #logic
-        headers = {'Content-Type': 'application/json'}
-        data = {
-            "flightdata": "your_flight_data",
-            "websessionid": "your_websession_id"
-        }
-        data = json.dump(data)
-        response2 = requests.put("http://localhost:5001/registersearch",data= data, headers = headers)
-        print(response2)
+
+        queue_name = 'searchflight'
+        channel.queue_declare(queue=queue_name)
+        message = 'Hello, RabbitMQ!'
+        channel.basic_publish(exchange='',
+                              routing_key=queue_name,
+                              body=message)
+
+        # logic
+        # headers = {'Content-Type': 'application/json'}
+        # data = {
+        #    "flightdata": "your_flight_data",
+        #    "websessionid": "your_websession_id"
+        # }
+        # data = json.dump(data)
         return response
-
-
-
-
-#@app.route("/listing",methods = ['GET'])
-#def listing():
-#    if request.method == 'GET':
-#        list ={'listelements': [{'string':'first element', "number":1},{"string": 'fifth element',"number":5},{"string": 'twentyfirst',"number":21}]}
-#        return jsonify(list)
 
 
 @app.route("/teapot")
 def teapot():
     return abort(418)
 
+
 if __name__ == '__main__':
+    service_name = "rabbitmqservice"
+    port_name = "amqp"
+
+    # Construct the environment variable names
+    service_host_env_var = f"{service_name.upper()}_SERVICE_HOST"
+    service_port_env_var = f"{service_name.upper()}_SERVICE_PORT_{port_name.upper()}"
+
+    # Access the values from environment variables
+    service_host = os.environ.get(service_host_env_var)
+    service_port = os.environ.get(service_port_env_var)
+    connection_params = pika.ConnectionParameters(service_host, service_port)
+    print(f"Connecting to RabbitMQ on {service_host}:{service_port}")
+    connection = pika.BlockingConnection(connection_params)
+    channel = connection.channel()
+
     app.run(host='0.0.0.0', port=5101)
