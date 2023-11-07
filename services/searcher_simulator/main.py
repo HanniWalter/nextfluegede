3fdf220e043cda1abcd4e100d65625c5eb90f9ca
HanniWalter <hannibali@gmx.de>
2023-11-07 22:02:21 +0100

    simple rabbitmq communication

--- services/searcher_simulator/main.py
+++ services/searcher_simulator/main.py
@@ -1,8 +1,9 @@
 import os
 import pika
+import time
+import threading
 
-
-def rabbitmq_channel():
+def rabbitmq_connection():
     service_name = "rabbitmqservice"
     port_name = "amqp"
 
@@ -16,22 +17,32 @@ def rabbitmq_channel():
     connection_params = pika.ConnectionParameters(service_host, service_port)
     print(f"Connecting to RabbitMQ on {service_host}:{service_port}")
     connection = pika.BlockingConnection(connection_params)
-    channel = connection.channel()
-    return channel
+    return connection
 
+def send_messages(rabbitmq):
+    channel = rabbitmq.channel()
+    time.sleep(1)
+    queue_name = 'searchflight'
+    channel.queue_declare(queue=queue_name) 
+    channel.confirm_delivery()
+    while True:        
+        # send message
+        b =channel.basic_publish(exchange='',
+                      routing_key=queue_name,
+                      body='Hello World!')
+        print("send hello world", b)
+        time.sleep(10)
 
 def main():
-    rabbitmq = rabbitmq_channel()
-
+    rabbitmq = rabbitmq_connection()
+    # Create a thread to send messages in the background
+    message_thread = threading.Thread(target=send_messages, args=(rabbitmq,))
+    message_thread.start()
+    
     while True:
-        queue_name = 'searchflight'
-        rabbitmq.queue_declare(queue=queue_name)
-        # get message
-        method_frame, header_frame, body = rabbitmq.basic_get(queue=queue_name)
-        if method_frame:
-            print(method_frame, header_frame, body)
-            rabbitmq.basic_ack(method_frame.delivery_tag)
+        pass
 
+    rabbitmq.close()
 
 if __name__ == "__main__":
     print("Starting provider manager")
